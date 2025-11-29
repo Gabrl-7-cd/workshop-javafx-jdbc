0a5975f134700f82af8cef79ee613014a4ec4392
GabrielSA <ga5227221@gmail.com>
2025-11-28 22:34:00 -0300

    About View

---
+++ src/gui/util/Constraints.java
@@ -0,0 +1,31 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+	// Função testa se o valor do textField é um inteiro;
+	public static void setTextFieldInteger(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	// Função diz qual é o limite de caracteres
+	public static void setTextFieldMaxLength(TextField txt, int max) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && newValue.length() > max) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+	// Função testa se o valor do textField é um ponto flutuante;
+	public static void setTextFieldDouble(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+}
